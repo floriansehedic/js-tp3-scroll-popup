7a5fb971492951264dd296cca83b8470e2f4faa6
Florian Séhédic <142261246+floriansehedic@users.noreply.github.com>
2023-09-16 21:53:21 +0200

    tp terminé

--- script.js
+++ script.js
@@ -9,13 +9,37 @@
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
 
 let scrollLvl = 0;
+let playOnce = true;
 window.addEventListener('scroll', () => {
-    //console.log('test');
 
+    // Navbar effect
     if (window.scrollY > scrollLvl) {
         navbar.style.height = "45px";
     } else {
         navbar.style.height = "90px";
-    }
+    };
     scrollLvl = window.scrollY;
+
+    //img
+    let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+    
+    if (scrollValue > 0.45) {
+        //console.log('test');
+        imgImprovise.style.opacity = 1;
+        imgImprovise.style.transform = "none";
+    };
+
+    //popup
+    if (scrollValue > 0.80 && playOnce) {
+        popup.style.opacity = 1;
+        popup.style.transform = "none";
+        playOnce = false;
+    }
 });
+
+closeBtn.addEventListener('click', () => {
+    //console.log('test');
+    popup.style.opacity = 1;
+    popup.style.transform = "translateX(600px)";
+});
+
